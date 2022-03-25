b33cc289815246f2b421ae5f975622bec042857b
guispinoza <guispinoza@gmail.com>
2022-03-25 12:23:24 +0100

    Just bad code

--- hw/03/cpp/src/Structures.h
+++ hw/03/cpp/src/Structures.h
@@ -1,7 +1,31 @@
 #pragma once
 
+
+#include "helpstructCGAL.h"
+//calling structures
+struct Face;
+struct Shell;
+struct Object;
+struct Vertex;
+// struct Shell_explorer;
+
+int shell_id = 0;
+
+struct Vertex {
+    // the coordinates of this vertex:
+    Point point;
+
+    // constructor without arguments
+    Vertex() : point(Point()) {}
+
+    // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
+    Vertex(const double &x, const double &y, const double &z) : point(Point(x, y, z)) {}
+};
+
 struct Face {
-    std::vector<unsigned long> vertices; // indices in a vector of points
+    std::vector<int> vertices; // indices in a vector of points
+    int a,b,c;
+
 };
 
 struct Shell {
@@ -12,3 +36,19 @@ struct Object {
     std::string id;
     std::vector<Shell> shells;
 };
+
+/* # STRUCT TO FIND THE SHELLS AND EXTRACT THEIR GEOMETRIES
+struct Shell_explorer {
+    std::vector<Point> vertices;
+    std::vector<std::vector<unsigned long>> faces;
+
+    void visit(Nef_polyhedron::Vertex_const_handle v) {}
+    void visit(Nef_polyhedron::Halfedge_const_handle he) {}
+    void visit(Nef_polyhedron::SHalfedge_const_handle she) {}
+    void visit(Nef_polyhedron::SHalfloop_const_handle shl) {}
+    void visit(Nef_polyhedron::SFace_const_handle sf) {}
+
+    void visit(Nef_polyhedron::Halffacet_const_handle hf) {
+      ... // do something to each half-face of a shell
+    }
+};*/
--- hw/03/cpp/src/main.cpp
+++ hw/03/cpp/src/main.cpp
@@ -1,24 +1,32 @@
-// some standard libraries that are helpfull for reading/writing text files
+// some standard libraries that are helpful for reading/writing text files
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
+#include <algorithm>
 #include "Structures.h"
 #include "helpstructCGAL.h"
-
+// #include "json.hpp" need to add the json.hpp file
 
 
 int main(int argc, const char * argv[]) {
-  std::string file_in = "input.obj";
-  std::string file_out_obj = "output.obj";
+  std::string file_in = "output2x3_guids";
+  std::string file_in_4 = "output4_guids";
+  std::string file_out_nef = "output_nef";
 
   // ## Read OBJ file ##
   std::ifstream stream_in;
   stream_in.open(file_in);
+  // ## Vectors to store elements
   std::vector<Point> vertices;
-  std::vector<Face> init_faces;
+  std::vector<Face> faces;
+  std::vector<Shell> shells;
+  std::vector<Object> objects;
 
+
+  //bool wtf = stream_in.is_open();
   if (stream_in.is_open()) {
     std::string line;
     while (getline(stream_in, line)) {
@@ -26,19 +34,24 @@ int main(int argc, const char * argv[]) {
       std::string word;
       iss >> word;
 
-      //read the vertices of the file
+      //## read the vertices ##
       if (word == "v") {
         std::vector<float> coordinates;
         while (iss >> word) coordinates.push_back(std::stof(word)); // stof: converts string to float
         if (coordinates.size() == 3) vertices.emplace_back(coordinates[0], coordinates[1], coordinates[2]);
-        else vertices.push_back(Vertex());
+        else vertices.emplace_back();
       }
 
       // ## read the faces ##
       if (word == "f") {
         std::vector<int> index;
         while (iss >> word) index.push_back(std::stoi(word)-1); //stoi converts str to int // we also substract 1 bc obj starts at 1 indexing and C++ structures start at 0 indexing
-        if (index.size() == 4) init_faces.emplace_back(Face{index[0], index[1], index[2], index[3]});
+        if (index.size() == 3) faces.emplace_back(Face{index[0], index[1], index[2], index[3]});
+      }
+
+      // ## read the objects ##
+      if (word == "g") {
+
       }
     }
   }
@@ -47,6 +60,19 @@ int main(int argc, const char * argv[]) {
   // # loading each object in the OBJ file(s) into a CGAL Nef polyhedron;
   // # processing the Nef polyhedra into a single big Nef polyhedron representing the space filled by the building’s floors, walls, windows and so on;
   // # extracting the geometries representing the building’s exterior surface and individual rooms;
+  // ## FUNCTION FOR THE SHELLS ##
+
+/*  Nef_polyhedron::Volume_const_iterator current_volume;
+  CGAL_forall_volumes(current_volume, big_nef) {
+    Nef_polyhedron::Shell_entry_const_iterator current_shell;
+    CGAL_forall_shells_of(current_shell, current_volume) {
+      Shell_explorer se;
+      Nef_polyhedron::SFace_const_handle sface_in_shell(current_shell);
+      big_nef.visit_shell_objects(sface_in_shell, se);
+      ...
+    }
+  }*/
+
   // # writing the geometries to a CityJSON file.
   
   return 0;
